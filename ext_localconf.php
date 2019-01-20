3b01b0bef63c036fe50def708d2f233a53f1299d
Tanel Põld <tanel@brightside.ee>
2019-01-20 15:37:27 +0800

    Add reliable condition to check if news is loaded

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,3 +8,13 @@ if($rootlinefields != '');
 }
 
 $rootlinefields .= 'tx_advancedtitle_prefix,tx_advancedtitle_sufix';
+
+if (TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news')){
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
+        advancedtitle.newsIsLoaded = 1
+    ');
+} else {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
+        advancedtitle.newsIsLoaded = 0
+    ');
+}
