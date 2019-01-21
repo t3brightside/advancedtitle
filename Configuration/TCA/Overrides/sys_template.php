f2812bae5678b802f113e0ac53ec3b7861fce6fe
Tanel Põld <tanel@brightside.ee>
2019-01-21 09:52:10 +0800

    Separate ext template for News article page

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,3 +2,8 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('advancedtitle', 'Configuration/TypoScript/', 'Advanced Page Title');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+  'advancedtitle',
+  'Configuration/TypoScript/News/',
+  'Advanced Page Title – News article page'
+);
