46e84897caf5104da273f6aff3f159cef0c3e10e
Tanel Põld <tanel@brightside.ee>
2019-04-26 13:59:02 +0300

    Bring back absolute page title option

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -16,12 +16,20 @@ $tempColumns = array(
 			'size' => '30',
 		],
 	],
+	'tx_advancedtitle_absolute' => [
+		'exclude' => 1,
+		'label' => 'LLL:EXT:advancedtitle/Resources/Private/Language/locallang_db.xml:tx_advancedtitle_absolute.title',
+		'config' => [
+			'type' => 'input',
+			'size' => '30',
+		],
+	],
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $tempColumns, 1);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
     'pages',
-    'tx_advancedtitle_prefix,tx_advancedtitle_sufix',
+    'tx_advancedtitle_prefix,tx_advancedtitle_sufix,tx_advancedtitle_absolute',
     '',
     'after:seo_title'
 );
--- ext_tables.sql
+++ ext_tables.sql
@@ -3,5 +3,6 @@
 #
 CREATE TABLE pages (
 	tx_advancedtitle_prefix tinytext,
-	tx_advancedtitle_sufix tinytext
+	tx_advancedtitle_sufix tinytext,
+	tx_advancedtitle_absolute tinytext
 );
