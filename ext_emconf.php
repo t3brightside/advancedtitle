22567b45032ed3d3c2c9289ccaa8cea0add82019
Tanel Põld <tanel@brightside.ee>
2021-03-25 12:48:45 -0300

    TYPO3 11 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@ $EM_CONF[$_EXTKEY] = array (
   'title' => 'Advanced Page Title',
   'description' => 'Adds page title prefix and suffix to SEO tab.',
   'category' => 'fe',
-  'version' => '0.3.0',
+  'version' => '0.3.1',
   'state' => 'stable',
   'uploadfolder' => false,
   'createDirs' => '',
@@ -13,8 +13,8 @@ $EM_CONF[$_EXTKEY] = array (
   'author_company' => 'Brightside OÜ / t3brightside.com',
   'constraints' => [
     'depends' => [
-      'typo3' => '9.5.0-10.4.99',
-      'seo' => '9.5.0-10.4.99',
+      'typo3' => '9.5.0-11.5.99',
+      'seo' => '9.5.0-11.5.99',
     ],
   ],
 );
