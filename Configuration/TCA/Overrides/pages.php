e30024a272c22dade8fb35cc87bb611384c5fbcd
Tanel Põld <tanel@brightside.ee>
2021-03-31 04:58:37 -0400

    lang files

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -2,7 +2,7 @@
 $tempColumns = array(
 	'tx_advancedtitle_prefix' => [
 		'exclude' => 1,
-		'label' => 'LLL:EXT:advancedtitle/Resources/Private/Language/locallang_db.xml:tx_advancedtitle_prefix.title',
+		'label' => 'LLL:EXT:advancedtitle/Resources/Private/Language/locallang_db.xlf:tx_advancedtitle_prefix.title',
 		'config'=> [
 			'type' => 'input',
 			'size' => '30',
@@ -10,7 +10,7 @@ $tempColumns = array(
 	],
 	'tx_advancedtitle_sufix' => [
 		'exclude' => 1,
-		'label' => 'LLL:EXT:advancedtitle/Resources/Private/Language/locallang_db.xml:tx_advancedtitle_sufix.title',
+		'label' => 'LLL:EXT:advancedtitle/Resources/Private/Language/locallang_db.xlf:tx_advancedtitle_sufix.title',
 		'config' => [
 			'type' => 'input',
 			'size' => '30',
@@ -18,7 +18,7 @@ $tempColumns = array(
 	],
 	'tx_advancedtitle_absolute' => [
 		'exclude' => 1,
-		'label' => 'LLL:EXT:advancedtitle/Resources/Private/Language/locallang_db.xml:tx_advancedtitle_absolute.title',
+		'label' => 'LLL:EXT:advancedtitle/Resources/Private/Language/locallang_db.xlf:tx_advancedtitle_absolute.title',
 		'config' => [
 			'type' => 'input',
 			'size' => '30',
