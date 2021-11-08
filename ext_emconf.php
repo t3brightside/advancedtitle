8cc5ad90fb7500113244b779c33584c9fb2aada7
Tanel Põld <tanel@brightside.ee>
2021-11-08 02:05:27 +0200

    v11.5 only

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,8 +13,8 @@ $EM_CONF[$_EXTKEY] = array (
   'author_company' => 'Brightside OÜ / t3brightside.com',
   'constraints' => [
     'depends' => [
-      'typo3' => '9.5.0-11.5.99',
-      'seo' => '9.5.0-11.5.99',
+      'typo3' => '11.5.0-11.5.99',
+      'seo' => '11.5.0-11.5.99',
     ],
   ],
 );
