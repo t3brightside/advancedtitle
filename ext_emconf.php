3327964db2d01559fc7fe36d0ddd51a91755a9c8
Tanel Põld <tanel@brightside.ee>
2021-11-10 07:43:53 +0200

    [TASK] Release 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,17 +3,14 @@ $EM_CONF[$_EXTKEY] = array (
   'title' => 'Advanced Page Title',
   'description' => 'Adds page title prefix and suffix to SEO tab.',
   'category' => 'fe',
-  'version' => '0.4.1',
+  'version' => '1.0.0',
   'state' => 'stable',
-  'uploadfolder' => false,
-  'createDirs' => '',
   'clearcacheonload' => true,
   'author' => 'Tanel Põld',
   'author_email' => 'tanel@brightside.ee',
   'author_company' => 'Brightside OÜ / t3brightside.com',
   'constraints' => [
     'depends' => [
-      'typo3' => '11.5.0-11.5.99',
       'seo' => '11.5.0-11.5.99',
     ],
   ],
