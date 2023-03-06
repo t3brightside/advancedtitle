820d3ad01a0607e5f47715989f99611f19a807e4
Tanel Põld <tanel@brightside.ee>
2023-03-06 16:27:45 +0200

    [TASK] Improve code readablity

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,8 @@
 <?php
-defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('advancedtitle', 'Configuration/TypoScript/', 'Advanced Page Title');
+ExtensionManagementUtility::addStaticFile(
+    'advancedtitle', 
+    'Configuration/TypoScript/', 
+    'Advanced Page Title'
+);
